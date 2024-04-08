ad760a123b2628046800d30a7ebea69e9dc3cdbb
Mohit Soni <mohitsoni9731@gmail.com>
2024-04-08 11:27:54 +0530

    Organize all the folder to create routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,11 @@
 import React from "react"
 import './App.css'
-import Header from "./Components/Files/Header/Header"
-import LandingPage from "./Components/Files/LandingPage/LandingPage"
-import WhatWeDo from "./Components/Files/What we Do/WhatWeDo"
-import Products from "./Components/Files/Products/Products"
-import OurMission from "./Components/Files/OurMission/OurMission"
-import Qualities from "./Components/Files/Qualities/Qualities"
-import Excellence from "./Components/Files/Excellence/Excellence"
-import CTA from "./Components/Files/CTA/CTA"
-import Footer from "./Components/Files/Footer/Footer"
+import Home from "./WebsitePages/Home/Home"
 
 function App() {
      return (
           <>
-               <div id="main">
-                    <div id="header">
-                         <Header />
-                    </div>
-                    <LandingPage />
-                    <div id="WhatWeDo">
-                         <WhatWeDo />
-                    </div>
-                    <div id="products">
-                         <Products />
-                    </div>
-                    <OurMission />
-                    <div id="qualities">
-                         <Qualities />
-                    </div>
-                    <Excellence />
-                    <div id="cta">
-                         <CTA />
-                    </div>
-                    <div id="footer">
-                         <Footer />
-                    </div>
-               </div>
+               <Home/>
           </>
      )
 }
---
+++ src/WebsitePages/Home/Home.jsx
@@ -0,0 +1,43 @@
+import React from "react"
+import "../../WebsitePages/Home/Home.css"
+import Header from "../../HomeComponents/Files/Header/Header"
+import LandingPage from "../../HomeComponents/Files/LandingPage/LandingPage"
+import WhatWeDo from "../../HomeComponents/Files/What we Do/WhatWeDo"
+import Products from "../../HomeComponents/Files/Products/Products"
+import OurMission from "../../HomeComponents/Files/OurMission/OurMission"
+import Qualities from "../../HomeComponents/Files/Qualities/Qualities"
+import Excellence from "../../HomeComponents/Files/Excellence/Excellence"
+import CTA from "../../HomeComponents/Files/CTA/CTA"
+import Footer from "../../HomeComponents/Files/Footer/Footer"
+
+function Home() {
+     return (
+          <>
+               <div id="main">
+                    <div id="header">
+                         <Header />
+                    </div>
+                    <LandingPage />
+                    <div id="WhatWeDo">
+                         <WhatWeDo />
+                    </div>
+                    <div id="products">
+                         <Products />
+                    </div>
+                    <OurMission />
+                    <div id="qualities">
+                         <Qualities />
+                    </div>
+                    <Excellence />
+                    <div id="cta">
+                         <CTA />
+                    </div>
+                    <div id="footer">
+                         <Footer />
+                    </div>
+               </div>
+          </>
+     )
+}
+
+export default Home
