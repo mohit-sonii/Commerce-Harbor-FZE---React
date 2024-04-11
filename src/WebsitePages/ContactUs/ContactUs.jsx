01de92ffd658bac6621930639a68549ace6fcbc7
Mohit Soni <mohitsoni9731@gmail.com>
2024-04-11 16:00:07 +0530

    Added LandingPage and contact options in Contact Page

--- src/WebsitePages/ContactUs/ContactUs.jsx
+++ src/WebsitePages/ContactUs/ContactUs.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import './ContactUs.css'
+import ContactOptions from "../../ContactComponents/Files/ContactOptions"
+import LandingPage from "../../ProductComponents/Files/LandingPage/LandingPage"
+import ContactBanner from "../../../assets/Thumbnail/Contact.png"
 const ContactUs = () => {
-  return (
-    <div>ContactUs</div>
-  )
+     return (
+          <>
+               <LandingPage title={"CONTACT US"} image={ContactBanner} />
+               <div id="ContactOptions">
+                    <ContactOptions />
+               </div>
+          </>
+     )
 }
 
 export default ContactUs
