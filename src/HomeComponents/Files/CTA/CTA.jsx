55139fff7760a21ec50f5199937e13583061254f
Mohit Soni <mohitsoni9731@gmail.com>
2024-04-14 16:45:39 +0530

    Added links to other buttons and added a smooth animtaion when the user enter in each page

--- src/HomeComponents/Files/CTA/CTA.jsx
+++ src/HomeComponents/Files/CTA/CTA.jsx
@@ -5,13 +5,12 @@ import "../../Styles/CTA/CTA.css"
 const CTA = () => {
      return (
           <div id='cta-section'>
-                    <div className="text-section">
-                         <p className='for-CTA'>WE ARE THE ONE WHO NEVER COMPROMISE ON THE QUALITY OF THE PRODUCTS</p>
-                    </div>
-                    <div className="button-section">
-                         <Button text={"Get a Free Quote"} btnColor={"red"} />
-
-                    </div>
+               <div className="text-section">
+                    <p className='for-CTA'>WE ARE THE ONE WHO NEVER COMPROMISE ON THE QUALITY OF THE PRODUCTS</p>
+               </div>
+               <div className="button-section">
+                    <Button destination={'/Commerce-Harbor-FZE---React/contact'} text={"Get a Free Quote"} btnColor={"red"} />
+               </div>
           </div>
      )
 }
--- src/HomeComponents/Files/OurMission/OurMission.jsx
+++ src/HomeComponents/Files/OurMission/OurMission.jsx
@@ -2,6 +2,7 @@
 import "../../Styles/OurMission/OurMission.css"
 import diamond from "../../../../assets/Icons/DiamondIcon.png"
 import Button from "../Button/Button"
+import { Link } from 'react-router-dom'
 
 
 const OurMission = () => {
@@ -13,8 +14,10 @@ const OurMission = () => {
                     <h2>COMMERCE HARBOR FZE</h2>
                     <p className='desc1'>We are committed to being a leading polymer distributor, providing exceptional value to our customers through the reliable supply of high-quality polymer materials. Our mission is to be a trusted partner in your success, delivering innovative solutions and personalized service that exceed your expectations.</p>
                     <div className="ourMissionButton">
-                         <Button text={"GET A QUOTE"} btnColor={"red"}/>
-                         <button className="transparent">OUR PRODUCTS</button>
+                         <Button  destination ={'/Commerce-Harbor-FZE---React/contact'} text={"GET A QUOTE"} btnColor={"red"} />
+                         <Link to="/Commerce-Harbor-FZE---React/products">
+                              <button className="transparent">OUR PRODUCTS</button>
+                         </Link>
                     </div>
                </div>
           </div>
