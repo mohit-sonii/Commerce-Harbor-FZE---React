1d8913de2eb853895677ea089c3b523bad197011
Mohit Soni <mohitsoni9731@gmail.com>
2024-06-28 17:21:04 +0530

    Transforming the REact into MERN project

--- src/main.jsx
+++ src/main.jsx
@@ -7,65 +7,14 @@ import Home from './WebsitePages/Home/Home.jsx'
 import Industries from './WebsitePages/Industries/Industries.jsx'
 import ContactUs from './WebsitePages/ContactUs/ContactUs.jsx'
 import Products from './WebsitePages/Products/Products.jsx'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Use BrowserRouter instead of createBrowserRouter
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
 
-// import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-
-
-
-// const router = createBrowserRouter([
-//      {
-//           path: "/Commerce-Harbor-FZE---React/", // toplevel element jiske andr nexting ho rhi hai
-//           element: <Home />,
-//           errorElement:<ErrorPage/>,
-//           children: [
-//                {
-//                     path:"", // kyuki slash pe bhi ho to kuch display hoega
-//                     element:<Home/>
-//                },
-//                {
-//                     path:"home",
-//                     element:<Home/>
-//                },
-//                {
-//                     path:"about",
-//                     element:<AboutUs/>
-//                },
-//                {
-//                     path:"contact",
-//                     element:<ContactUs/>
-//                },
-//                {
-//                     path:"industries",
-//                     element:<Industries/>
-//                },
-//                {
-//                     path:"products",
-//                     element:<Products/>
-//                }
-//           ]
-//      }
-// ])
-
-
-
-// const router = createBrowserRouter(
-//      createRoutesFromElements(
-//           <Route path='/'element={<Home/>}>
-//                <Route path='' element={<Home/>}/>
-//                <Route path='about' element={<AboutUs/>}/>
-//                <Route path='contact' element={<ContactUs/>}/>
-//                <Route path='industries' element={<Industries/>}/>
-//                <Route path='products' element={<Products/>}/>
-//           </Route>
-//      )
-// )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
      <React.StrictMode>
           <Router>
                <Routes>
-                    <Route path="/Commerce-Harbor-FZE---React/" element={<App />}>
+                    <Route path="/Commerce-Harbor-FZE---MERN/" element={<App />}>
                          <Route index element={<Home />} />
                          <Route path="about" element={<AboutUs />} />
                          <Route path="home" element={<Home />} />
