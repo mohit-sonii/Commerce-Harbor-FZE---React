ff3ec7c5824ab60311a012ad9be8c5323deefef7
Mohit Soni <mohitsoni9731@gmail.com>
2024-06-28 19:19:04 +0530

    trying #3 to deploy backned on vercel

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,8 +4,11 @@ import cors from 'cors'
 import form from './routes/form.routes.js'
 const app = express()
 
-app.use('/',form)
-app.use(cors())
+app.use('/', form)
+app.use(cors({
+     origin: 'https://commerce-harbor-fze-mern.vercel.app/',
+     credentials: true
+}))
 
 app.use((err, req, res, next) => {
      const statusCode = err.statusCode || 500;
